30f12eea8578c467856a55d9bd16222f9f5a37e9
kellel14 <kellel14@yahoo.com>
2019-05-22 10:58:27 -0400

    SpringBoot_203(revisted)

--- src/main/java/com/example/demo/Song.java
+++ src/main/java/com/example/demo/Song.java
@@ -12,44 +12,44 @@ public class Song {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getArtist() {
+        return artist;
+    }
+
     public void setArtist(String artist) {
         this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
     }
 
     public void setAlbum(String album) {
         this.album = album;
     }
 
+    public int getRating() {
+        return rating;
+    }
+
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     public void setYear(int year) {
